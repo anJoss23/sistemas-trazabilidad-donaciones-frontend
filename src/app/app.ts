9a9384e62c6b9c28e2ceccdc30642647042c9afb
JoseReyesR <Josereyesrubio95@gmail.com>
2026-06-26 00:54:54 -0500

    agregado mostrar usuario en menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+// 1. NUEVO: Importamos el AuthService
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +13,19 @@ import { CommonModule } from '@angular/common';
 export class App {
   showMenu: boolean = false;
 
-  constructor(private router: Router) {
+  // 2. NUEVO: Inyectamos private authService: AuthService en el constructor
+  constructor(private router: Router, private authService: AuthService) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         // Mostramos el menú en todas las rutas excepto login
         this.showMenu = event.url !== '/login' && event.url !== '/';
       }
     });
+  }
+
+  // 3. NUEVO: Creamos el "Getter" para leer los datos del token en la vista
+  get usuarioActual() {
+    return this.authService.obtenerDatosUsuario();
   }
 
   logout() {
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,4 +26,47 @@ export class AuthService {
   cerrarSesion(): void {
     localStorage.removeItem('token');
   }
+  //  MÉTODO PARA LEER LOS DATOS DEL TOKEN
+  /*obtenerDatosUsuario(): any {
+    const token = this.obtenerToken();
+    if (token) {
+      try {
+        // El token JWT tiene 3 partes separadas por un punto. Los datos están en el medio .
+        const payload = token.split('.')[1];
+        // Decodificamos el texto de Base64
+        const decodedPayload = atob(payload);
+        return JSON.parse(decodedPayload);
+      } catch (error) {
+        console.error('Error al decodificar el token', error);
+        return null;
+      }
+
+    }
+    return null;
+  }*/
+
+    obtenerDatosUsuario(): any {
+    const token = this.obtenerToken();
+    if (token) {
+      try {
+        let payload = token.split('.')[1];
+
+        // 1. Reemplazamos caracteres especiales de Base64-URL por si el token los trae
+        payload = payload.replace(/-/g, '+').replace(/_/g, '/');
+
+        // 2. Decodificación segura para tildes y eñes (UTF-8)
+        const decodedPayload = decodeURIComponent(
+          window.atob(payload).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+          }).join('')
+        );
+
+        return JSON.parse(decodedPayload);
+      } catch (error) {
+        console.error('Error al decodificar el token', error);
+        return null;
+      }
+    }
+    return null;
+  }
 }
